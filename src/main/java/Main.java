766c759d7c85820dd58ebca809ac1231951bc3da
Musza21 <patryk.musza@wp.pl>
2025-11-17 08:45:27 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+/*
+Kod bazowy programu Commit4_0: 
+• Program dodaje do prostej bazy danych (pliku db.txt) dane odnośnie Studentów.
+• Studenci dodawani są w klasie Main.
+• Wszyscy studenci są wypisywani na końcu klasy Main.
+• Klasa Service obsługuje odczyt i zapis do pliku bazy danych.
+• Klasa Student reprezentuje pojedynczego studenta (Imię, Wiek).
+*/
+
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    try {
+      var input = new Scanner(System.in);
+      System.out.println("Podaj imię: ");
+      String name = input.nextLine();
+      System.out.println("Podaj wiek: ");
+      int age = input.nextInt();
+      input.close();
+      Service s = new Service();
+
+      s.addStudent(new Student(name, age));
+
+      var students = s.getStudents();
+      for(Student current : students) {
+        System.out.println(current.ToString());
+      }
+    } catch (IOException e) {
+
+    }
+  }
+}
