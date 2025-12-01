2aac5ed6a2f4e23cb57df1163a960d277c65a4ba
Musza21 <patryk.musza@wp.pl>
2025-12-01 09:48:47 +0100

    P5_1_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@ class Main {
                 System.out.println("\n MENU ");
                 System.out.println("1 - Lista studentów");
                 System.out.println("2 - Dodawnie studenta");
+                System.out.println("3 - Szukaj studenta po imieniu");
                 System.out.println("0 - Wyjście");
                 System.out.print("Wybierz opcję: ");
 
@@ -18,35 +19,43 @@ class Main {
                 input.nextLine();
 
                 switch (wybor) {
-                    case 1:
-                    var students = s.getStudents();
-                    System.out.println("\n- Lista studentów ---");
-                    for (Student current : students) {
-                        System.out.println(current);
 
-                    }
-                    break;
+                    case 1:
+                        var students = s.getStudents();
+                        System.out.println("\n- Lista studentów ---");
+                        for (Student current : students) {
+                            System.out.println(current);
+                        }
+                        break;
 
                     case 2:
-                    System.out.print("Podaj imię: ");
-                    String name = input.nextLine();
+                        System.out.print("Podaj imię: ");
+                        String name = input.nextLine();
 
-                    System.out.print("Podaj nazwisko: ");
-                    String surname = input.nextLine();
-                    System.out.println("Podaj date urodzenia w formacie RRRR-MM-DD");
-                    System.out.print("Podaj Date urodzenia: ");
-                    String date = input.nextLine();
-                    
-                        
+                        System.out.print("Podaj nazwisko: ");
+                        String surname = input.nextLine();
 
-                    System.out.print("Podaj wiek: ");
-                    int age = input.nextInt();
-                    input.nextLine();
+                        System.out.print("Podaj date urodzenia (RRRR-MM-DD): ");
+                        String date = input.nextLine();
 
-                    s.addStudent(new Student(name, surname, date, age));
-                    System.out.println("Dodano studenta!\n");
-                    break;
+                        System.out.print("Podaj wiek: ");
+                        int age = input.nextInt();
+                        input.nextLine();
 
+                        s.addStudent(new Student(name, surname, date, age));
+                        System.out.println("Dodano studenta!\n");
+                        break;
+
+                    case 3:
+                        System.out.print("Podaj imię studenta: ");
+                        String search = input.nextLine();
+                        Student found = s.findStudentByName(search);
+
+                        if (found != null)
+                            System.out.println("Znaleziono: " + found);
+                        else
+                            System.out.println("Nie znaleziono studenta o takim imieniu.");
+                        break;
 
                     case 0:
                         System.out.println("Zamykanie programu...");
@@ -62,10 +71,4 @@ class Main {
             }
         }
     }
-
-	@Override
-	public String toString() {
-		return "Main []";
-	}
 }
-
