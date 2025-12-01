960679fcf9ea90724ac30c0f970febfa76ac0ec4
Musza21 <patryk.musza@wp.pl>
2025-12-01 09:27:22 +0100

    P5_1

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,28 +2,36 @@ public class Student {
 
     private String Name;
     private String Surname;
+    private String Date;
     private int Age;
 
-    public Student(String name, String surname, int age) {
+    public Student(String name, String surname,String Data, int age) {
         this.Name = name;
         this.Surname = surname;
+        this.Date = Data;
         this.Age = age;
     }
 
     public String getName() { return Name; }
     public String getSurname() { return Surname; }
+    public String getDate() { return Date; }
     public int getAge() { return Age; }
 
     @Override
     public String toString() {
-        return Name + " " + Surname + " " + Age;
+        return Name + " " + Surname + " "+ Date+ " " + Age;
     }
-
     public static Student Parse(String str) {
         String[] data = str.split(" ");
-        if (data.length != 3)
-            return new Student("Parse", "Error", -1);
+        if (data.length != 4)
+            return new Student("Parse", "Error", "0000-00-00", -1);
 
-        return new Student(data[0], data[1], Integer.parseInt(data[2]));
+        return new Student(
+                data[0],
+                data[1],
+                data[2],
+                Integer.parseInt(data[3])
+            );
     }
 }
+
